e7261295c8a36e6d5d3090b0b70f83aacbfe589e
Zhukova Nina <avokadotata@gmail.com>
2021-12-04 14:26:38 +0300

    calendar

--- src/datepicker.js
+++ src/datepicker.js
@@ -4,38 +4,30 @@ import 'jquery-ui/ui/widgets/datepicker.js'
 import 'jquery-ui/ui/i18n/datepicker-ru.js'
 
 
-
-
 $(function() {
-    $('.datepicker').datepicker({
+    $('#datepicker').datepicker({
         range: 'period', 
         numberOfMonths: 1,
         minDate: 0,
         showButtonPanel: true,
         currentText:'очистить',
+        closeText : 'применить',
         onSelect: function(dateText, inst, extensionRange) {
           // extensionRange - объект расширения
+          
             $('[name=startDate]').val(extensionRange.startDateText);
             $('[name=endDate]').val(extensionRange.endDateText);
         }
     });
-    $('.datepicker').datepicker('setDate', ['+4d', '+8d']);
+
+    $('#datepicker').datepicker('setDate', ['+4d', '+8d']);
   
     // объект расширения (хранит состояние календаря)
-    var extensionRange = $('.datepicker').datepicker('widget').data('datepickerExtensionRange');
+    var extensionRange = $('#datepicker').datepicker('widget').data('datepickerExtensionRange');
     if(extensionRange.startDateText) $('[name=startDate]').val(extensionRange.startDateText);
     if(extensionRange.endDateText) $('[name=endDate]').val(extensionRange.endDateText);
 
 });
-$(function() {
-$('.datepicker').hide()
 
-$('[name=startDate]').on('click',function(){
-    $('.datepicker').show()
-})
-$('[name=endDate]').on('click',function(){
-    $('.datepicker').show()
-})
-})
 
 
