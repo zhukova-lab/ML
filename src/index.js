1ac61bf6abe7547d343986ad26c2c1b65a6f0d93
Zhukova Nina <avokadotata@gmail.com>
2021-12-05 11:45:01 +0300

    create page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+require('webpack-jquery-ui');
+require('webpack-jquery-ui/css');
+
 import '@fortawesome/fontawesome-free/js/all.js'
 import './styles/styles.less'
+import './scripts/modal.js'
+import './scripts/switch.js'
+import './datepicker.js'
+import './scripts/dropdowns.js'
+
+
